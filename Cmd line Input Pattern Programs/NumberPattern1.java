f9a2c1e4ca048a4c57a145cb757da17bf205aff9
ANDRA TULASI LAKSHMI TANYA <150415501+TanyaCoder-27@users.noreply.github.com>
2024-02-15 20:58:23 +0530

    Add files via upload

---
+++ Cmd line Input Pattern Programs/NumberPattern1.java
@@ -0,0 +1,22 @@
+class NumberPattern1
+{
+	public static void main(String args[])
+	{
+		if(args.length==0)
+			System.out.println("give input");
+		else{
+			int n=Integer.parseInt(args[0]);
+			int num=0;
+			for(int i=1;i<=n;i++)
+			{
+				num++;
+				for(int j=1;j<=i;j++)
+				{	
+					
+					System.out.print(num);
+				}
+				System.out.println();
+			}
+		}
+	}
+}
